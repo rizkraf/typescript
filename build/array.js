88dc83674ac4aa7b75678a010a37466c762fce32
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-08-10 15:03:41 +0700

    array tuple

--- build/array.js
+++ build/array.js
@@ -6,3 +6,8 @@ let array2;
 array2 = ["satu", "dua", "tiga"];
 let array3;
 array3 = [1, "dua", true, {}];
+// array tuple
+let biodata;
+biodata = ["Bandung", 123]; // ok
+biodata = ["Jakarta", true]; // error
+biodata = ["Cimahi", 212, false]; // error
--- src/array.ts
+++ src/array.ts
@@ -7,3 +7,9 @@ array2 = ["satu", "dua", "tiga"];
 
 let array3: any[];
 array3 = [1, "dua", true, {}];
+
+// array tuple
+let biodata: [string, number];
+biodata = ["Bandung", 123]; // ok
+biodata = ["Jakarta", true]; // error
+biodata = ["Cimahi", 212, false]; // error
