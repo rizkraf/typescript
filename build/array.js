ef552e9cf95754ccfa4eb0724fa4d750a815ab0a
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-08-10 14:54:28 +0700

    array

---
+++ build/array.js
@@ -0,0 +1,8 @@
+"use strict";
+// array
+let array;
+array = [1, 2, 3];
+let array2;
+array2 = ["satu", "dua", "tiga"];
+let array3;
+array3 = [1, "dua", true, {}];
---
+++ src/array.ts
@@ -0,0 +1,9 @@
+// array
+let array: number[];
+array = [1, 2, 3];
+
+let array2: string[];
+array2 = ["satu", "dua", "tiga"];
+
+let array3: any[];
+array3 = [1, "dua", true, {}];
