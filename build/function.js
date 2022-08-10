868089deea965751f778626c9bf0fee8c6468aeb
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-08-10 15:39:21 +0700

    function

---
+++ build/function.js
@@ -0,0 +1,13 @@
+"use strict";
+// tipe data pada balikan function
+function getName() {
+    return "Hello, my name is Rizky";
+}
+console.log(getName());
+function getAge() {
+    return 19;
+}
+function printName() {
+    console.log("print name");
+}
+printName();
---
+++ src/function.ts
@@ -0,0 +1,17 @@
+// tipe data pada balikan function
+
+function getName(): string {
+  return "Hello, my name is Rizky";
+}
+
+console.log(getName());
+
+function getAge(): number {
+  return 19;
+}
+
+function printName(): void {
+  console.log("print name");
+}
+
+printName();
