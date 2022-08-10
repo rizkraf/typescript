d7c3599364b14bb5d3302ea5a824d005e9a216c9
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-08-10 14:45:19 +0700

    number & boolean

--- build/typeDasar.js
+++ build/typeDasar.js
@@ -1,3 +1,12 @@
 "use strict";
+// string
 let nama = "Rizky";
 nama = "Rafi";
+// number
+let umur;
+umur = 30;
+umur = 19;
+// boolean
+let isAnime;
+isAnime = true;
+isAnime = false;
--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -1,3 +1,13 @@
+// string
 let nama: string = "Rizky";
-
 nama = "Rafi";
+
+// number
+let umur: number;
+umur = 30;
+umur = 19;
+
+// boolean
+let isAnime: boolean;
+isAnime = true;
+isAnime = false;
