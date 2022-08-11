296d2f2ca44a4a68215a95974d11dad5da6268ca
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-08-11 07:22:48 +0700

    object

---
+++ build/object.js
@@ -0,0 +1,6 @@
+"use strict";
+// object
+let user = {
+    name: "Rizky",
+    age: 19,
+};
---
+++ src/object.ts
@@ -0,0 +1,11 @@
+// object
+
+type User = {
+  name: string;
+  age: number;
+};
+
+let user: User = {
+  name: "Rizky",
+  age: 19,
+};
